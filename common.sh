5d96337eee3e8fadc81bbfc75d0334f40e1c5e20
mapi <rfriends2017@ymail.ne.jp>
2025-01-24 19:48:08 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -100,10 +100,12 @@ elif [ $distro = "centos" ] || [ $distro = "stream9" ]; then
 else
   # ubuntu
   sudo $cmd update
-  sudo $cmd unzip nano vim at wget curl tzdata \
-  php-cli php-xml php-zip php-mbstring php-json php-curl php-intl \
-  openssh-server \
-  cron p7zip-full atomicparsley ffmpeg iproute2
+  sudo $cmd unzip nano vim at wget curl tzdata
+  sudo $cmd cron p7zip-full iproute2
+  sudo $cmd php-cli php-zip php-xml php-mbstring php-json php-curl php-intl
+  sudo $cmd openssh-server
+  sudo $cmd atomicparsley
+  sudo $cmd ffmpeg
   sudo $cmd chromium-browser
   sudo $cmd chromium
 fi

4767a6e4a42c4c6168c1ef2964f85ddfd72a9836
mapi <rfriends2017@ymail.ne.jp>
2025-01-24 09:10:44 +0900

    Update install_stream9.sh

--- install_stream9.sh
+++ install_stream9.sh
@@ -42,6 +42,6 @@ export optlighttpd="on2"
 export optsamba="on"
 export optvimrc="on"
 #
-sh install_rfriends3.sh 2>&1 | tee install_rfriends3.log
+sh common.sh 2>&1 | tee common.log
 # -----------------------------------------
 # finish

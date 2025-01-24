6e83a590168b39bffdd4f49fdd5d0c51b31cee75
mapi <rfriends2017@ymail.ne.jp>
2025-01-24 20:55:48 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -128,7 +128,6 @@ echo vimrc $optvimrc
 
 if [ $optvimrc = "on" ]; then
 cd $homedir
-mv -n .vimrc .vimrc.org
 cat <<EOF > .vimrc
 set encoding=utf-8
 set fileencodings=iso-2022-jp,euc-jp,sjis,utf-8

89ed4a08979bb0ccadf2911e0eaf82be2141e6f0
mapi <rfriends2017@ymail.ne.jp>
2025-01-25 14:04:19 +0900

    Create install_lite_ubuntu.sh

---
+++ install_lite_ubuntu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# =========================================
+# install rfriends for ubuntu
+# only lighttpd
+# =========================================
+# 1.0 2025/01/03 new
+# 1.1 2025/01/24 mod
+
+ver=1.1
+echo start $ver
+echo
+# -----------------------------------------
+export distro="ubuntu"
+export cmd="apt-get install -y"
+export cmdupdate="apt-get update"
+
+#export user=user
+#export group=user
+#export port=8000
+#export homedir=`sh -c 'cd && pwd'`
+#export PREFIX=""
+
+export optlighttpd="on"
+export optsamba="off"
+export optvimrc="off"
+
+export lighttpd="lighttpd"
+export smbd="smbd"
+export atd="atd"
+export cron="cron"
+#
+sh common.sh 2>&1 | tee common.log
+# -----------------------------------------
+# finish
+echo
+echo finished
+# -----------------------------------------

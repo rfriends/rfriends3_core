3c2c86a21f122d38c48c0f3672d317f00e13f030
mapi <rfriends2017@ymail.ne.jp>
2025-04-15 23:42:46 +0900

    Update install_alpine.sh

--- install_alpine.sh
+++ install_alpine.sh
@@ -2,10 +2,6 @@
 # =========================================
 # install rfriends for alpine
 #
-#　注意事項
-# １．/etc/at.allow にユーザを追加してください。
-# ２．/etc/apk/repositoriesにtestingを追加してください。
-# @testing https://dl-cdn.alpinelinux.org/alpine/edge/testing
 # =========================================
 # 1.0 2025/04/15 new
 #
@@ -13,11 +9,17 @@ ver=1.0
 echo start $ver
 echo
 # -----------------------------------------
+testing="@testing https://dl-cdn.alpinelinux.org/alpine/edge/testing"
+sudo grep $testing /etc/apk/repositories
+if [ $? -eq 1 ]; then
+  echo $testing | sudo tee -a /etc/apk/repositories
+fi
+# -----------------------------------------
 export distro="alpine"
 export cmd="apk add"
 export cmdupdate="apk update"
 
-#export user=`whoami`
+export user=`whoami`
 #export group=`groups | cut -d ' ' -f 1`
 #export port=8000
 #export homedir=`sh -c 'cd && pwd'`
@@ -39,6 +41,11 @@ apk --update-cache add tzdata
 cp /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 
 sh common.sh 2>&1 | tee common.log
+# -----------------------------------------
+sudo grep -x "$user" /etc/at.allow
+if [ $? -eq 1 ]; then
+  echo $user | sudo tee -a /etc/at.allow
+fi
 # -----------------------------------------
 # finish
 echo

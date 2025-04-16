3b160f7e8c38b3f7ce7130910db199152b7b95cf
mapi <rfriends2017@ymail.ne.jp>
2025-04-16 09:49:09 +0900

    Update common_bsd.sh

--- common_bsd.sh
+++ common_bsd.sh
@@ -174,10 +174,16 @@ fi
 # systemd or service
 # -----------------------------------------
 if [ $sys -eq 1 ]; then
-  #sudo systemctl enable $atd
+  sudo systemctl enable $atd
+  sudo systemctl start  $atd
+  sudo systemctl status $atd
   sudo systemctl enable $cron
+  sudo systemctl start  $cron
+  sudo systemctl status $cron
 else 
-  #sudo service $atd restart
+  sudo service $atd enable
+  sudo service $atd restart
+  sudo service $cron enable
   sudo service $cron restart
 fi
 # -----------------------------------------

30469254e1b57a87bc82b9caf52a4c8ec2841759
mapi <rfriends2017@ymail.ne.jp>
2025-04-17 20:09:40 +0900

    Create install_oracle.sh

---
+++ install_oracle.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# =========================================
+# install rfriends3 for stream
+# =========================================
+# Rfriends (radiko radiru録音ツール)
+# 1.0 2022/03/18
+# 1.1 2022/12/22 stream9
+# 2.1 2024/12/16 github
+# 2.3 2025/01/24 fix
+ver=2.3
+# -----------------------------------------
+echo start $ver
+echo
+# -----------------------------------------
+# enforceをPermissiveに設定
+#grep "^SELINUX=enforcing" /etc/selinux/config> /dev/null
+#sudo sed -i "/^SELINUX=enforcing/c SELINUX=Permissive" /etc/selinux/config
+sudo setenforce 0
+# タイムゾーンを東京に
+timedatectl set-timezone Asia/Tokyo
+# -----------------------------------------
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+echo
+echo RPM Fusion リポジトリを追加
+echo
+sudo dnf -y install epel-release
+sudo dnf -y config-manager --set-enabled crb
+
+sudo dnf -y install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm -y
+sudo dnf -y install --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm -y
+
+sudo dnf makecache
+sudo dnf install -y ffmpeg ffmpeg-devel
+# -----------------------------------------
+export distro="stream"
+export cmd="dnf install -y"
+
+#export user=user
+#export group=user
+#export port=8000
+#export homedir=`sh -c 'cd && pwd'`
+#export PREFIX=""
+
+export optlighttpd="on2"
+export optsamba="on"
+export optvimrc="on"
+
+export lighttpd="lighttpd"
+export smbd="smb"
+export atd="atd"
+export cron="crond"
+#
+dnf update
+sh common.sh 2>&1 | tee common.log
+# -----------------------------------------
+# finish

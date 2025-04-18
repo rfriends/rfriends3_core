24ab9acd526189815d305703cacf293002db2097
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 19:51:41 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -16,7 +16,8 @@
 # 4.5 2025/02/27 arch
 # 4.6 2025/04/15 suse
 # 4.7 2025/04/15 alpine
-ver=4.7
+# 4.8 2025/04/18 mod
+ver=4.8
 # -----------------------------------------
 echo
 echo start install_common $ver
@@ -132,49 +133,25 @@ else
   # ubuntu
   sudo $cmd chromium-browser
 fi
-# -----------------------------------------
-echo
-echo vimrc
-echo
-# -----------------------------------------
-echo vimrc $optvimrc
-
-if [ $optvimrc = "on" ]; then
-cd $homedir
-cat <<EOF > .vimrc
-set encoding=utf-8
-set fileencodings=iso-2022-jp,euc-jp,sjis,utf-8
-set fileformats=unix,dos,mac
-EOF
-chmod 644 .vimrc
-fi
 # =========================================
 echo
 echo install rfriends3
 echo
 # =========================================
 echo rfriends3
-
-cd $homedir
-cp -f $curdir/$SCRIPT .
-unzip -q -o $SCRIPT
-
-mkdir -p tmp
-cat <<EOF > rfriends3/config/usrdir.ini
-usrdir = "$homedir/rfriends3/usr/"
-tmpdir = "$homedir/tmp/"
-EOF
+sh rfriends3.sh
+#
+sh at.sh
+#
+sh cron.sh
 # -----------------------------------------
-# systemd or service
+echo
+echo vimrc
+echo
 # -----------------------------------------
-if [ $sys -eq 1 ]; then
-  sudo systemctl enable $atd
-  sudo systemctl enable $cron
-  sudo systemctl start $atd
-  sudo systemctl start $cron
-else 
-  sudo service $atd restart
-  sudo service $cron restart
+echo vimrc $optvimrc
+if [ $optvimrc = "on" ]; then
+  sh vimrc.sh
 fi
 # -----------------------------------------
 echo
@@ -182,33 +159,8 @@ echo install samba
 echo
 # -----------------------------------------
 echo samba $optsamba
-
 if [ $optsamba = "on" ]; then
-sudo $cmd samba
-sudo mkdir -p /var/log/samba
-sudo chown root:adm /var/log/samba
-
-cd $curdir/skel
-sed -e s%rfriendshomedir%$homedir%g smb.conf.skel > smb.conf
-sed -i s%rfriendsuser%$user%g smb.conf
-sudo cp -f smb.conf $PREFIX/etc/samba/smb.conf
-sudo chown root:root $PREFIX/etc/samba/smb.conf
-
-mkdir -p $homedir/smbdir/usr2/
-cat <<EOF > $homedir/rfriends3/config/usrdir.ini
-usrdir = "$homedir/smbdir/usr2/"
-tmpdir = "$homedir/tmp/"
-EOF
-
-# -----------------------------------------
-if [ $sys -eq 1 ]; then
-  sudo systemctl enable $smbd
-  sudo systemctl restart $smbd
-  systemctl status $smbd
-else 
-  sudo service $smbd restart
-  service $smbd status
-fi
+  sh samba.sh
 fi
 # -----------------------------------------
 echo
@@ -216,132 +168,22 @@ echo install lighttpd
 echo
 # -----------------------------------------
 echo lighttpd $optlighttpd
-# ========================================= on
 if [ $optlighttpd = "on" ]; then
-sudo $cmd lighttpd php-cgi
-
-if [ $distro = "suse" ] || [ $distro = "alpine" ]; then
-  sudo $cmd lighttpd-mod_webdav
-else
-  sudo $cmd lighttpd-mod-webdav
-fi
-
-cd $curdir/skel
-sed -e s%rfriendshomedir%$homedir%g 15-fastcgi-php.conf.skel > 15-fastcgi-php.conf
-sudo cp -f 15-fastcgi-php.conf $PREFIX/etc/lighttpd/conf-available/15-fastcgi-php.conf
-sudo chown root:root $PREFIX/etc/lighttpd/conf-available/15-fastcgi-php.conf
-
-sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel > lighttpd.conf
-sed -i s%rfriendsuser%$user%g   lighttpd.conf
-sed -i s%rfriendsgroup%$group%g lighttpd.conf
-sed -i s%rfriendsport%$port%g   lighttpd.conf
-sudo cp -f lighttpd.conf $PREFIX/etc/lighttpd/lighttpd.conf
-sudo chown root:root $PREFIX/etc/lighttpd/lighttpd.conf
-
-mkdir -p $homedir/lighttpd/uploads/
-cd $homedir/rfriends3/script/html
-ln -nfs temp webdav
-
-sudo lighttpd-enable-mod fastcgi
-sudo lighttpd-enable-mod fastcgi-php
-echo lighttpd > $homedir/rfriends3/rfriends3_boot.txt
-# -----------------------------------------
-if [ $sys -eq 1 ]; then
-  sudo systemctl enable $lighttpd
-  sudo systemctl restart $lighttpd
-  systemctl status $lighttpd
-else 
-  sudo service $lighttpd restart
-  service $lighttpd status
-fi
-fi
-# ========================================= on2
-if [ $optlighttpd = "on2" ]; then
-sudo $cmd lighttpd php-cgi
-sudo $cmd fcgi
-sudo $cmd lighttpd-fastcgi
-
-cd $curdir
-
-if [ ! -d $PREFIX/etc/lighttpd/conf.d ]; then
-  sudo mkdir -p $PREFIX/etc/lighttpd/conf.d
-  sudo cp -f conf.d/* $PREFIX/etc/lighttpd/conf.d/
-fi
-
-cd $curdir/skel
-sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel2 > lighttpd.conf
-if [ $distro = "arch" ]; then
-  sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel3 > lighttpd.conf
-  sudo mkdir -p $PREFIX/var/log/lighttpd
-  sudo chown $user:$group $PREFIX/var/log/lighttpd
-fi
-sed -i s%rfriendsuser%$user%g lighttpd.conf
-sed -i s%rfriendsgroup%$group%g lighttpd.conf
-sed -i s%rfriendsport%$port%g lighttpd.conf
-sudo cp -f lighttpd.conf $PREFIX/etc/lighttpd/lighttpd.conf
-sudo chown root:root $PREFIX/etc/lighttpd/lighttpd.conf
-#
-# modules
-sudo cp -f modules.conf.skel2 $PREFIX/etc/lighttpd/modules.conf
-sudo chown root:root $PREFIX/etc/lighttpd/modules.conf
-#
-# fastcgi
-sudo cp -f fastcgi.conf.skel2 $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
-sudo chown root:root $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
-#
-# webdav
-sudo cp -f webdav.conf.skel2 $PREFIX/etc/lighttpd/conf.d/webdav.conf
-sudo chown root:root $PREFIX/etc/lighttpd/conf.d/webdav.conf
-cd $homedir/rfriends3/script/html
-ln -nfs temp webdav
-#
-mkdir -p $homedir/lighttpd/uploads/
-mkdir -p $homedir/lighttpd/sockets/
-#
-echo lighttpd > $homedir/rfriends3/rfriends3_boot.txt
-# -----------------------------------------
-if [ $sys -eq 1 ]; then
-  sudo systemctl enable $lighttpd
-  sudo systemctl restart $lighttpd
-  systemctl status $lighttpd
-else 
-  sudo service $lighttpd restart
-  service $lighttpd status
-fi
+  sh lighttpd.sh
+elif [ $optlighttpd = "on2" ]; then
+  sh lighttpd2.sh
 fi
 # =========================================
 echo
-cat $homedir/rfriends3/_Rfriends3
+echo result of install rfriends3
 echo
-echo "`cat /etc/os-release | grep PRETTY_NAME`"
-echo "distro : $distro"
-if [ $sys -eq 1 ]; then
-  echo "type : systemd" 
-else 
-  echo "type : initd"
-fi
-echo
-echo samba    : $optsamba
-echo lighttpd : $optlighttpd
-echo vimrc    : $optvimrc
-echo
-echo user  : $user
-echo group : $group
-echo port  : $port
-echo
-echo home    directry : $homedir
-echo current directry : $curdir
-echo PREFIX : $PREFIX
-echo
-#ip=`hostname -I | cut -d " " -f 1`
-ip=`ip a | grep "inet " | grep -v "127.0.0.1" | sed -e 's/^ *//' | cut -d " " -f 2`
-echo
-echo IP address : $ip
-
+sh result.sh
 # -----------------------------------------
 # finish
 # -----------------------------------------
 echo `date`
 echo end install common
 echo
+# -----------------------------------------
+exit 0
 # -----------------------------------------

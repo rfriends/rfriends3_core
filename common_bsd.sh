e7fb8e244798ec8525d373c5d4dc108cb986d425
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 19:36:11 +0900

    Update common_bsd.sh

--- common_bsd.sh
+++ common_bsd.sh
@@ -164,34 +164,7 @@ fi
 echo
 echo resukt of install rfriends3
 echo
-# =========================================
-cat $homedir/rfriends3/_Rfriends3
-echo
-echo "`cat /etc/os-release | grep PRETTY_NAME`"
-echo "distro : $distro"
-if [ $sys -eq 1 ]; then
-  echo "type : systemd" 
-else 
-  echo "type : initd"
-fi
-echo
-echo vimrc    : $optvimrc
-echo samba    : $optsamba
-echo lighttpd : $optlighttpd
-echo
-echo user  : $user
-echo group : $group
-echo port  : $port
-echo
-echo home    directry : $homedir
-echo current directry : $curdir
-echo PREFIX : $PREFIX
-echo
-#ip=`hostname -I | cut -d " " -f 1`
-ip=`$ipcmd | grep "inet " | grep -v "127.0.0.1" | sed -e 's/^ *//' | cut -d " " -f 2`
-echo
-echo IP address : $ip
-
+sh result.sh
 # -----------------------------------------
 # finish
 # -----------------------------------------

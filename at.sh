d0d0fc6121cd2b814b377e7c08cfa084bd8af950
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 19:23:59 +0900

    Create at.sh

---
+++ at.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# at
+#
+if [ $sys -eq 1 ]; then
+  #sudo systemctl enable $atd
+  #sudo systemctl start  $atd
+  #sudo systemctl status $atd
+else 
+  #sudo service $atd enable
+  #sudo service $atd restart
+fi
+
+exit 0

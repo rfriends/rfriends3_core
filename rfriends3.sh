2ff97eee224a74d26657c0579074a5f188d473c5
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 18:56:45 +0900

    Create rfriends3.sh

---
+++ rfriends3.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# rfriends3
+#
+cd $homedir
+cp -f $curdir/$SCRIPT .
+unzip -q -o $SCRIPT
+
+mkdir -p tmp
+cat <<EOF > rfriends3/config/usrdir.ini
+usrdir = "$homedir/rfriends3/usr/"
+tmpdir = "$homedir/tmp/"
+EOF
+# -----------------------------------------
+# systemd or service
+# -----------------------------------------
+if [ $sys -eq 1 ]; then
+  sudo systemctl enable $atd
+  sudo systemctl enable $cron
+  sudo systemctl start $atd
+  sudo systemctl start $cron
+else 
+  sudo service $atd restart
+  sudo service $cron restart
+fi
+exit 0

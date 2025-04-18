42aa71806c5ebe9e22651ac28f5bdda837776114
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 15:31:59 +0900

    Update common.sh

--- common.sh
+++ common.sh
@@ -309,6 +309,59 @@ else
 fi
 fi
 # -----------------------------------------
+if [ $optlighttpd = "on3" ]; then
+sudo $cmd lighttpd php-cgi
+sudo $cmd lighttpd-fastcgi
+
+if [ ! -d $PREFIX/etc/lighttpd/conf.d ]; then
+  sudo mkdir -p $PREFIX/etc/lighttpd/conf.d
+fi
+
+cd $curdir
+
+if [ $distro = "arch" ] || [ $distro = "alpine" ]; then
+  sudo cp -f conf.d/* $PREFIX/etc/lighttpd/conf.d/
+fi
+
+cd $curdir/skel
+sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel2 > lighttpd.conf
+sed -i s%rfriendsuser%$user%g lighttpd.conf
+sed -i s%rfriendsgroup%$group%g lighttpd.conf
+sed -i s%rfriendsport%$port%g lighttpd.conf
+sudo cp -f lighttpd.conf $PREFIX/etc/lighttpd/lighttpd.conf
+sudo chown root:root $PREFIX/etc/lighttpd/lighttpd.conf
+#
+# modules
+sudo cp -f modules.conf.skel2 $PREFIX/etc/lighttpd/modules.conf
+sudo chown root:root $PREFIX/etc/lighttpd/modules.conf
+#
+# fastcgi
+sudo cp -f fastcgi.conf.skel2 $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
+sudo chown root:root $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
+#
+# webdav
+sudo cp -f webdav.conf.skel2 $PREFIX/etc/lighttpd/conf.d/webdav.conf
+sudo chown root:root $PREFIX/etc/lighttpd/conf.d/webdav.conf
+cd $homedir/rfriends3/script/html
+ln -nfs temp webdav
+#
+mkdir -p $homedir/lighttpd/uploads/
+mkdir -p $homedir/lighttpd/sockets/
+cd $homedir/rfriends3/script/html
+ln -nfs temp webdav
+
+echo lighttpd > $homedir/rfriends3/rfriends3_boot.txt
+# -----------------------------------------
+if [ $sys -eq 1 ]; then
+  sudo systemctl enable $lighttpd
+  sudo systemctl restart $lighttpd
+  systemctl status $lighttpd
+else 
+  sudo service $lighttpd restart
+  service $lighttpd status
+fi
+fi
+# -----------------------------------------
 echo
 cat $homedir/rfriends3/_Rfriends3
 echo

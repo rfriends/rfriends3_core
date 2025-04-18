f792bea967e2b81b6582bc1f5efbaa08a2a016b7
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 19:45:19 +0900

    Update at.sh

--- at.sh
+++ at.sh
@@ -2,12 +2,12 @@
 # at
 #
 if [ $sys -eq 1 ]; then
-  #sudo systemctl enable $atd
-  #sudo systemctl start  $atd
-  #sudo systemctl status $atd
+  sudo systemctl enable $atd
+  sudo systemctl start  $atd
+  sudo systemctl status $atd
 else 
-  #sudo service $atd enable
-  #sudo service $atd restart
+  sudo service $atd enable
+  sudo service $atd restart
 fi
 
 exit 0

d9b8762104a547d80eb76a3ae9c2127c22216e15
mapi <rfriends2017@ymail.ne.jp>
2025-04-18 19:17:48 +0900

    Update common_bsd_new.sh

--- common_bsd_new.sh
+++ common_bsd_new.sh
@@ -139,16 +139,6 @@ echo
 # =========================================
 echo rfriends3
 sh rfriends3.sh
-
-cd $homedir
-cp -f $curdir/$SCRIPT .
-unzip -q -o $SCRIPT
-
-mkdir -p tmp
-cat <<EOF > rfriends3/config/usrdir.ini
-usrdir = "$homedir/rfriends3/usr/"
-tmpdir = "$homedir/tmp/"
-EOF
 # -----------------------------------------
 # at
 # -----------------------------------------
@@ -191,42 +181,8 @@ echo install samba
 echo
 # -----------------------------------------
 echo samba $optsamba
-
 if [ $optsamba = "on" ]; then
-sudo $cmd $samba
-
-#sudo mkdir -p /var/log/samba
-#sudo chown root:adm /var/log/samba
-
-cd $curdir/skel_bsd
-sed -e s%rfriendshomedir%$homedir%g smb4.conf.skel > smb4.conf
-sed -i .bak s%rfriendsuser%$user%g smb4.conf
-sudo cp -f smb4.conf $PREFIX/etc/smb4.conf
-#sudo chown root:root $PREFIX/etc/smb4.conf
-
-mkdir -p /var/db/samba4/private
-mkdir -p /var/log/samba4
-
-sudo chmod 755 /var/run/samba4
-sudo chmod 700 /var/db/samba4/private
-sudo mkdir /var/run/samba4/fd
-
-mkdir -p $homedir/smbdir/usr2/
-
-cat <<EOF > $homedir/rfriends3/config/usrdir.ini
-usrdir = "$homedir/smbdir/usr2/"
-tmpdir = "$homedir/tmp/"
-EOF
-
-# -----------------------------------------
-if [ $sys -eq 1 ]; then
-  sudo systemctl enable $smbd
-  sudo systemctl restart $smbd
-  systemctl status $smbd
-else 
-  sudo service $smbd restart
-  sudo service $smbd status
-fi
+  sh samba.sh
 fi
 # -----------------------------------------
 echo
@@ -236,6 +192,10 @@ echo
 echo lighttpd $optlighttpd
 
 if [ $optlighttpd = "on3" ]; then
+  sh lighttpd3.sh
+fi
+
+
 sudo $cmd lighttpd
 #sudo $cmd ${php}-extensions 
 #sudo $cmd lighttpd-fastcgi

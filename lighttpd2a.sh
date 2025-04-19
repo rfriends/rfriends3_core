5ce8ee5209fe6cdf6aba15cfb991fdfcd01f5ceb
mapi <rfriends2017@ymail.ne.jp>
2025-04-19 21:24:25 +0900

    Update lighttpd2a.sh

--- lighttpd2a.sh
+++ lighttpd2a.sh
@@ -8,20 +8,8 @@ sudo $cmd lighttpd-fastcgi
 
 cd $curdir
 
-if [ ! -d $PREFIX/etc/lighttpd/conf.d ]; then
-  sudo mkdir -p $PREFIX/etc/lighttpd/conf.d
-fi
-sudo cp -f conf.d/* $PREFIX/etc/lighttpd/conf.d/
-
 cd $curdir/skel
-sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel2 > lighttpd.conf
-if [ $distro = "arch" ]; then
-  sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel2a > lighttpd.conf
-  sudo mkdir -p $PREFIX/var/log/lighttpd
-  sudo chown $user:$group $PREFIX/var/log/lighttpd
-fi
-sudo chown $user:$group $PREFIX/var/lib/lighttpd
-
+sed -e s%rfriendshomedir%$homedir%g lighttpd.conf.skel2a > lighttpd.conf
 sed -i s%rfriendsuser%$user%g lighttpd.conf
 sed -i s%rfriendsgroup%$group%g lighttpd.conf
 sed -i s%rfriendsport%$port%g lighttpd.conf
@@ -42,6 +30,15 @@ sudo chown root:root $PREFIX/etc/lighttpd/conf.d/webdav.conf
 cd $homedir/rfriends3/script/html
 ln -nfs temp webdav
 #
+
+sudo mkdir -p $PREFIX/var/log/lighttpd
+sudo mkdir -p $PREFIX/var/lib/lighttpd
+sudo mkdir -p $PREFIX/var/cache/lighttpd
+
+sudo chown $user:$group $PREFIX/var/log/lighttpd
+sudo chown $user:$group $PREFIX/var/lib/lighttpd
+sudo chown $user:$group $PREFIX/var/cache/lighttpd
+
 mkdir -p $homedir/lighttpd/uploads/
 mkdir -p $homedir/lighttpd/sockets/
 #

b5a2fd6c15a8566eb7eb9b7299e166c6610ce604
mapi <rfriends2017@ymail.ne.jp>
2025-04-21 06:09:52 +0900

    Update at.sh

--- at.sh
+++ at.sh
@@ -6,8 +6,8 @@ if [ $sys -eq 1 ]; then
   sudo systemctl start  $atd
   sudo systemctl status $atd
 else 
-  sudo service $atd enable
   sudo service $atd restart
+  sudo service $atd status
 fi
 
 exit 0

bb3544b1f0b893947d4c8d3b2396c5c8146d2632
mapi <rfriends2017@ymail.ne.jp>
2025-04-21 18:44:27 +0900

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -36,6 +36,7 @@ export app_chromium="chromium-browser"
 export app_atomicparsley="atomicparsley"
 #
 sudo apt-get update
+sudo apt-get install chromium
 sh common.sh 2>common.err | tee common.log
 echo --- commmon.err
 cat common.err

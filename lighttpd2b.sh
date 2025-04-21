063186eea9fc6cfca2d91bc5bc2c37bd7781d740
mapi <rfriends2017@ymail.ne.jp>
2025-04-21 14:29:38 +0900

    Update lighttpd2b.sh

--- lighttpd2b.sh
+++ lighttpd2b.sh
@@ -1,44 +1,38 @@
 #!/bin/sh
 # lighttpd on2b
-# 2025/04/20
+# 2025/04/21
 #
 onver="on2b"
-
-sudo $cmd lighttpd
-sudo $cmd php-cgi
-#sudo $cmd fcgi
-sudo $cmd lighttpd-fastcgi
 # -----------------------------------------
 # log_root
-sudo mkdir -p $PREFIX/var/log/lighttpd
-sudo chown $user:$group $PREFIX/var/log/lighttpd
+sudo mkdir -p $log_root
+sudo chown $user:$group $log_root
 
 # state_dir
-sudo mkdir -p $PREFIX/run/lighttpd
-sudo chown $user:$group $PREFIX/run/lighttpd
+sudo mkdir -p $state_dir
+sudo chown $user:$group $state_dir
 
 # home_dir
-sudo mkdir -p $PREFIX/var/lib/lighttpd
-sudo mkdir -p $PREFIX/var/lib/lighttpd/uploads/
-sudo mkdir -p $PREFIX/var/lib/lighttpd/sockets/
+sudo mkdir -p $home_dir
+sudo mkdir -p $home_dir/uploads/
+sudo mkdir -p $home_dir/sockets/
 
-sudo chown $user:$group $PREFIX/var/lib/lighttpd
-sudo chown $user:$group $PREFIX/var/lib/lighttpd/uploads/
-sudo chown $user:$group $PREFIX/var/lib/lighttpd/sockets/
+sudo chown $user:$group $home_dir
+sudo chown $user:$group $home_dir/uploads/
+sudo chown $user:$group $home_dir/sockets/
 
 # conf_dir
-sudo mkdir -p $PREFIX/etc/lighttpd
-sudo chown $user:$group $PREFIX/etc/lighttpd
-sudo mkdir -p $PREFIX/etc/lighttpd/conf.d
-sudo chown $user:$group $PREFIX/etc/lighttpd/conf.d
+sudo mkdir -p $conf_dir
+sudo chown $user:$group $conf_dir
+sudo mkdir -p $conf_dir/conf.d
+sudo chown $user:$conf_dir/conf.d
 
 # cache_dir
-sudo mkdir -p $PREFIX/var/cache/lighttpd
-sudo chown $user:$group $PREFIX/var/cache/lighttpd
-
+sudo mkdir -p $cache_dir
+sudo chown $user:$group $cache_dir
 # -----------------------------------------
 cd $curdir
-sudo cp -f conf.d/* $PREFIX/etc/lighttpd/conf.d/
+sudo cp -f conf.d/* $conf_dir/conf.d/
 
 cd $curdir/skel
 
@@ -49,31 +43,29 @@ sed -e s%rfriendsgroup%$group%g lighttpd.confc > lighttpd.confd
 sed -e s%rfriendsport%$port%g   lighttpd.confd > lighttpd.confe
 
 sed -e s%rfriendslog_root%/var/log/lighttpd%g lighttpd.confe > lighttpd.conf1
-sed -e s%rfriendsstate_dir%/run/lighttpd%g    lighttpd.conf1 > lighttpd.conf2
-sed -e s%rfriendshome_dir%/var/lib/lighttpd%g lighttpd.conf2 > lighttpd.conf3
-sed -e s%rfriendsconf_dir%/etc/lighttpd%g     lighttpd.conf3 > lighttpd.conf4
-sed -e s%rfriendscache_dir%/var/cache/lighttpd%g lighttpd.conf4 > lighttpd.conf5
+sed -e s%rfriendsstate_dir%/var/run%g         lighttpd.conf1 > lighttpd.conf2
+sed -e s%rfriendshome_dir%/var/run/lighttpd%g lighttpd.conf2 > lighttpd.conf3
+sed -e s%rfriendsconf_dir%/usr/local/etc/lighttpd%g lighttpd.conf3 > lighttpd.conf4
+sed -e s%rfriendscache_dir%/var/cache/lighttpd%g    lighttpd.conf4 > lighttpd.conf5
 
-sudo cp -f lighttpd.conf5 $PREFIX/etc/lighttpd/lighttpd.conf
+sudo cp -f lighttpd.conf5 $conf_dir/lighttpd.conf
 #
 # modules
-sudo cp -f modules.conf.skel2b $PREFIX/etc/lighttpd/modules.conf
-#sudo chown root:root $PREFIX/etc/lighttpd/modules.conf
+sudo cp -f modules.conf.skel2b $conf_dir/modules.conf
+#sudo chown root:root $conf_dir/modules.conf
 #
 # fastcgi
-sed -e s%rfriendsphpdir%$phpdir%g fastcgi.conf.skel2b > fastcgi.conf
-sudo cp -f fastcgi.conf $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
-#sudo chown root:root $PREFIX/etc/lighttpd/conf.d/fastcgi.conf
+sed -e s%rfriendsbindir%$PREFIX$phpdir%g fastcgi.conf.skel2b > fastcgi.conf
+sudo cp -f fastcgi.conf $conf_dir/conf.d/fastcgi.conf
+#sudo chown root:root $conf_dir/conf.d/fastcgi.conf
 #
 # webdav
-sudo cp -f webdav.conf.skel2b $PREFIX/etc/lighttpd/conf.d/webdav.conf
-#sudo chown root:root $PREFIX/etc/lighttpd/conf.d/webdav.conf
+sudo cp -f webdav.conf.skel2b $conf_dir/conf.d/webdav.conf
+#sudo chown root:root $conf_dir/conf.d/webdav.conf
 cd $homedir/rfriends3/script/html
 ln -nfs temp webdav
 
 echo lighttpd > $homedir/rfriends3/rfriends3_boot.txt
-
-sudo touch $PREFIX/etc/lighttpd/$onver
 # -----------------------------------------
 if [ $sys -eq 1 ]; then
   sudo systemctl enable $lighttpd
@@ -84,4 +76,5 @@ else
   service $lighttpd status
 fi
 
+sudo touch $conf_dir/$onver
 exit 0

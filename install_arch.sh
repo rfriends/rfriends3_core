72b210df4e19f218b00b9b909c4a8f719ebe9c64
mapi <rfriends2017@ymail.ne.jp>
2025-04-21 09:44:02 +0900

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -25,7 +25,6 @@ export cmd="pacman -S --noconfirm"
 #export port=8000
 #export homedir=`sh -c 'cd && pwd'`
 #export PREFIX=""
-#export bindir="/usr/bin"
 #export phpdir="/usr/bin"
 
 export optlighttpd="on2"

39d168e5d816fe8bf90b332199a7d37fce5543a4
mapi <rfriends2017@ymail.ne.jp>
2025-04-22 13:35:51 +0900

    Update lighttpd1a.sh

--- lighttpd1a.sh
+++ lighttpd1a.sh
@@ -44,6 +44,8 @@ ln -nfs temp webdav
 
 sudo lighttpd-enable-mod fastcgi
 sudo lighttpd-enable-mod fastcgi-php
+mkdir $homedir/lighttpd
+
 echo lighttpd > $homedir/rfriends3/rfriends3_boot.txt
 # -----------------------------------------
 if [ $sys -eq 1 ]; then

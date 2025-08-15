6d27573b5752dafb4e1eca393f9b04609c0c1499
mapi <rfriends2017@ymail.ne.jp>
2025-08-15 19:32:02 +0900

    Update httpd.sh

--- httpd.sh
+++ httpd.sh
@@ -12,24 +12,13 @@ cd $curdir/skel_apache
 
 # sed -i は使用しないこと
 
-sed -e s%rfriendshomedir%$homedir%g apache2.conf.skel > apache2.conf
-sed -e s%rfriendsuser%$user%g   envvars.skel > envvars
-sed -e s%rfriendsport%$port%g ports.conf.skel > ports.conf
+sed -e s%rfriendshomedir%$homedir%g httpd.conf.skel > httpd.conf1
+sed -e s%rfriendsuser%$user%g httpd.conf1 > httpd.conf2
+sed -e s%rfriendsport%$port%g httpd.conf2 > httpd.conf
 
-sed -e s%rfriendshomedir%$homedir%g 000-default.conf.skel > 000-default.conf1
-sed -e s%rfriendsport%$port%g 000-default.conf1 > 000-default.conf
-
-sudo cp -f apache2.conf $conf_dir/apache2.conf
-sudo cp -f envvars      $conf_dir/envvars
-sudo cp -f ports.conf   $conf_dir/ports.conf
-
-sudo cp -f 000-default.conf $conf_dir/sites-enabled/000-default.conf
+sudo cp -f httpd.conf $conf_dir/httpd.conf
 
 # webdav
-
-sudo a2enmod dav
-sudo a2enmod dav_fs
-sudo a2enmod allowmethods
 
 cd $homedir/rfriends3/script/html
 ln -nfs temp webdav
@@ -39,9 +28,9 @@ if [ $sys -eq 1 ]; then
   sudo systemctl restart $apache2
   sudo systemctl status  $apache2
 else 
-  sudo service $lighttpd restart
-  sudo service $lighttpd enable
-  sudo service $lighttpd status
+  sudo service $apache2 restart
+  sudo service $apache2 enable
+  sudo service $apache2 status
 fi
 sudo touch $conf_dir/$onver
 exit 0

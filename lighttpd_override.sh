e8cfd40280096b3d28c9816a79bde2e8f66e536d
mapi <rfriends2017@ymail.ne.jp>
2026-04-24 18:24:49 +0900

    Update copy command to use lighttpd_override.conf

--- lighttpd_override.sh
+++ lighttpd_override.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo mkdir -p /etc/systemd/system/lighttpd.service.d/
-sudo cp override.conf /etc/systemd/system/lighttpd.service.d/override.conf
+sudo cp lighttpd_override.conf /etc/systemd/system/lighttpd.service.d/override.conf
 sudo systemctl daemon-reload
 sudo systemctl restart lighttpd

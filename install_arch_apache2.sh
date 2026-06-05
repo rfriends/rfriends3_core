a45a85dead1c40ae3114962711dd06d6dbe54c3c
mapi <rfriends2017@ymail.ne.jp>
2026-06-05 09:35:52 +0900

    Update SSH service commands in install script

--- install_arch_apache2.sh
+++ install_arch_apache2.sh
@@ -67,8 +67,9 @@ cat common.err
 # firewall 無効化（必要に応じて有効にしてください）
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
+# または
 sudo ufw disable
 #
-sudo systemctl start $app_openssh
-sudo systemctl enable $app_openssh
+sudo systemctl start sshd
+sudo systemctl enable sshd
 # finish

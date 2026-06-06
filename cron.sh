1e2194a33b833bcce2ad4dc23015494a02ca2c9a
mapi <rfriends2017@ymail.ne.jp>
2026-06-07 00:21:46 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -2,13 +2,16 @@
 # cron
 #
 sudo $cmd $app_cron
+# 10 -> 1
+sudo crontab -l | sed 's|^\*/10\(.*/usr/libexec/atrun\)|*\1|' | sudo crontab -
+sudo crontab -l | grep atrun
 #
 if [ $sys -eq 1 ]; then
   sudo systemctl enable $cron
   sudo systemctl start  $cron
   sudo systemctl status $cron
 else 
-  sudo service $cron restart
-  sudo service $cron status
+  sudo /etc/rc.d/cron restart
+  sudo /etc/rc.d/cron status
 fi
 exit 0

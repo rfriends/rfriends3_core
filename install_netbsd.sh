de26352fa6f5ef8e68fca25d2515e9d8d89f9bf4
mapi <rfriends2017@ymail.ne.jp>
2026-06-06 23:24:26 +0900

    Add install script for rfriends on FreeBSD

---
+++ install_netbsd.sh
@@ -0,0 +1,55 @@
+#!/usr/local/bin/bash
+# =========================================
+# install rfriends for freeBSD
+# =========================================
+# 1.0 2025/03/04 new
+# 1.1 2025/04/20 bindir
+# 1.2 2026/06/06 samba422
+
+ver=1.2
+echo start $ver
+echo
+sudo pkg update
+if [ $? != 0 ]; then
+  echo
+  echo install_freebsd.sh
+  echo 実行するshを間違っていないか確認してください。
+  echo
+  cat /etc/os-release
+  exit 1
+fi
+# -----------------------------------------
+export distro="freebsd"
+export cmd="pkg install -y"
+export cmdupdate="pkg update"
+
+#export user=`whoami`
+#export group=`groups | cut -d ' ' -f 1`
+#export port=8000
+#export homedir=`sh -c 'cd && pwd'`
+export PREFIX=""
+export phpdir="/usr/local/bin"
+
+export optlighttpd="on2b"
+export optsamba="on"
+export optvimrc="on"
+
+export php="php84"
+export lighttpd="lighttpd"
+# samba4のみ
+export samba="samba422"
+export smbd="samba_server"
+export atd="atd"
+export cron="cron"
+export ipcmd="ifconfig"
+#
+
+
+sh common_bsd.sh 2>common_bsd.err | tee common_bsd.log
+echo --- commmon_bsd.err
+cat common_bsd.err
+# -----------------------------------------
+# finish
+echo
+echo finished
+# -----------------------------------------

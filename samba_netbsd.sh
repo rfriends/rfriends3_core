1f24d9007d5416b0b289a102b7ff283738282d5e
mapi <rfriends2017@ymail.ne.jp>
2026-06-07 00:23:27 +0900

    Add samba_netbsd.sh script for Samba configuration

---
+++ samba_netbsd.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# samba
+#
+# 1.1 2025/08/16 /usr/local/etc/smb4.conf
+
+sudo $cmd $samba
+
+#sudo mkdir -p /var/log/samba
+#sudo chown root:adm /var/log/samba
+
+cd $curdir/skel
+sed -e s%rfriendshomedir%$homedir%g smb4.conf.skel > smb4.conf0
+sed -e s%rfriendsuser%$user%g smb4.conf0 > smb4.conf
+#sudo cp -f smb4.conf $PREFIX/etc/smb4.conf
+sudo cp -f smb4.conf $PREFIXi/usr/local/etc/smb4.conf
+#sudo chown root:root $PREFIX/etc/smb4.conf
+rm smb4.conf0
+
+mkdir -p /var/db/samba4/private
+mkdir -p /var/log/samba4
+
+sudo chmod 755 /var/run/samba4
+sudo chmod 700 /var/db/samba4/private
+sudo mkdir /var/run/samba4/fd
+
+mkdir -p $homedir/smbdir/usr2/
+
+cat <<EOF > $homedir/rfriends3/config/usrdir.ini
+usrdir = "$homedir/smbdir/usr2/"
+tmpdir = "$homedir/tmp/"
+EOF
+
+# -----------------------------------------
+if [ $sys -eq 1 ]; then
+  sudo systemctl enable $smbd
+  sudo systemctl restart $smbd
+  sudo systemctl status $smbd
+else 
+  sudo service $smbd restart
+  sudo service $smbd status
+fi
+
+exit 0

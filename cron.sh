88847cc1cd7ff731a8de22857b97712ff3cc615c
mapi <rfriends2017@ymail.ne.jp>
2026-06-07 18:44:43 +0900

    Update cron.sh for conditional cron job handling
    Refactor cron job setup for NetBSD and other distros.

--- cron.sh
+++ cron.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # cron
 #
-sudo $cmd $app_cron
+
+if [ $distro = "netbsd" ]; then
 # 10 -> 1
-sudo crontab -l | sed 's|^\*/10\(.*/usr/libexec/atrun\)|*\1|' | sudo crontab -
-sudo crontab -l | grep atrun
+  sudo crontab -l | sed 's|^\*/10\(.*/usr/libexec/atrun\)|*\1|' | sudo crontab -
+  sudo crontab -l | grep atrun
+else
+  sudo $cmd $app_cron
+fi
 #
 if [ $sys -eq 1 ]; then
   sudo systemctl enable $cron

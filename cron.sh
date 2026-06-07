d1e35fd83c0b7406a349a4d4a9d2ba848ad9416a
mapi <rfriends2017@ymail.ne.jp>
2026-06-07 18:51:14 +0900

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 # cron
 #
-
 if [ $distro = "netbsd" ]; then
-# 10 -> 1
+  # 10 -> 1
   sudo crontab -l | sed 's|^\*/10\(.*/usr/libexec/atrun\)|*\1|' | sudo crontab -
   sudo crontab -l | grep atrun
-else
-  sudo $cmd $app_cron
+  sudo /etc/rc.d/cron restart
+  sudo /etc/rc.d/cron status
+  exit 0
 fi
+
+sudo $cmd $app_cron
 #
 if [ $sys -eq 1 ]; then
   sudo systemctl enable $cron
   sudo systemctl start  $cron
   sudo systemctl status $cron
 else 
-  if [ $distro != "netbsd" ]; then
-    sudo service cron restart
-    sudo service cron status
-  else
-    sudo /etc/rc.d/cron start
-    sudo /etc/rc.d/cron status
-  fi
+  sudo service $cron restart
+  sudo service $cron status
 fi
 exit 0

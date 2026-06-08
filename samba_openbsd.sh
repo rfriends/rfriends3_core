4346fd273371ce88d05166e8b62614bdf2d955e9
mapi <rfriends2017@ymail.ne.jp>
2026-06-09 01:54:48 +0900

    Update samba_openbsd.sh

--- samba_openbsd.sh
+++ samba_openbsd.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 # samba
 #
-# 1.0 2026/06/08 /usr/local/etc/smb4.conf
+# 1.0 2026/06/08 /etc/smb4.conf
 
 sudo $cmd $samba
 
 cd $curdir/skel
 sed -e s%rfriendshomedir%$homedir%g smb4.conf_netbsd.skel > smb4.conf0
 sed -e s%rfriendsuser%$user%g smb4.conf0 > smb4.conf
-
-if [ $distro = "netbsd" ]; then
-  sudo cp -f smb4.conf $PREFIX/usr/pkg/etc/samba/smb.conf
-fi
-if [ $distro = "openbsd" ]; then
-  sudo cp -f smb4.conf $PREFIX/etc/samba/smb.conf
-fi
+sudo cp -f smb4.conf $PREFIX/etc/samba/smb.conf
 rm smb4.conf0
 
 mkdir -p $homedir/smbdir/usr2/
@@ -30,19 +24,8 @@ if [ $sys -eq 1 ]; then
   sudo systemctl restart $smbd
   sudo systemctl status $smbd
 else 
-  if [ $distro = "netbsd" ]; then
-    sudo cp /usr/pkg/share/examples/rc.d/smbd /etc/rc.d/
-    sudo /etc/rc.d/smbd start
-    sudo /etc/rc.d/smbd status
- elif [ $distro = "openbsd" ]; then
-    sudo cp /usr/local/share/examples/rc.d/smbd /etc/rc.d/
-    sudo /etc/rc.d/smbd start
-    sudo /etc/rc.d/smbd check
- else
-    sudo service $smbd enable
-    sudo service $smbd restart
-    sudo service $smbd status
-  fi
+  sudo rcctl start smbd
+  sudo rcctl check smbd
 fi
 
 exit 0

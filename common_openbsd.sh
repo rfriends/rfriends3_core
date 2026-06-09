f9a7f3b7fd6ee1646dc4f851b37044b6f7f656ef
mapi <rfriends2017@ymail.ne.jp>
2026-06-09 13:26:09 +0900

    Update common_openbsd.sh

--- common_openbsd.sh
+++ common_openbsd.sh
@@ -162,13 +162,7 @@ $sucmd ln -sf /etc/php-${phpv}.sample/zip.ini /etc/php-${phpv}/zip.ini
 $sucmd ln -sf /etc/php-${phpv}.sample/intl.ini /etc/php-${phpv}/intl.ini
 $sucmd ln -sf /etc/php-${phpv}.sample/ssh2.ini /etc/php-${phpv}/ssh2.ini 
 
-#SAMPLE_FILE="/etc/php-${phpv}.sample"
 INI_FILE="/etc/php-${phpv}.ini"
-
-#if [ ! -f "$INI_FILE" ] && [ -f "$SAMPLE_FILE" ]; then
-#    cp "$SAMPLE_FILE" "$INI_FILE"
-#fi
-echo $INI_FILE
 if [ -f "$INI_FILE" ]; then
     sed -i '' 's/^allow_url_fopen = Off/allow_url_fopen = On/' "$INI_FILE"
 fi
@@ -223,7 +217,7 @@ echo lighttpd $optlighttpd
 if [ $optlighttpd = "on3" ]; then  
   $sucmd $cmd lighttpd--
 
-  export conf_dir="$PREFIX$/etc"
+  export conf_dir="/etc"
   sh lighttpd3.sh
 fi
 # =========================================

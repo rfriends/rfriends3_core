9e537a6ebd6a2c24d37f45ddb4e8536813d89750
mapi <rfriends2017@ymail.ne.jp>
2026-06-09 13:41:52 +0900

    Update common_openbsd.sh

--- common_openbsd.sh
+++ common_openbsd.sh
@@ -162,10 +162,8 @@ $sucmd ln -sf /etc/php-${phpv}.sample/zip.ini /etc/php-${phpv}/zip.ini
 $sucmd ln -sf /etc/php-${phpv}.sample/intl.ini /etc/php-${phpv}/intl.ini
 $sucmd ln -sf /etc/php-${phpv}.sample/ssh2.ini /etc/php-${phpv}/ssh2.ini 
 
-INI_FILE="/etc/php-${phpv}.ini"
-if [ -f "$INI_FILE" ]; then
-    sed -i '' 's/^allow_url_fopen = Off/allow_url_fopen = On/' "$INI_FILE"
-fi
+
+sed -i '' 's/^allow_url_fopen = Off/allow_url_fopen = On/' "/etc/php-${phpv}.ini"
 # -----------------------------------------
 #$sucmd $cmd tzdata
 #$sucmd $cmd iproute2

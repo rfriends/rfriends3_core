81e6ff5e60f4eccc0858f455922b9590ed8081c3
mapi <rfriends2017@ymail.ne.jp>
2026-06-11 16:27:09 +0900

    Add samba_slackware.sh script for Samba configuration

---
+++ samba_slackware.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# samba
+#
+# 1.0 2026/06/08 /etc/smb4.conf
+
+sudo $cmd $samba
+
+cd $curdir/skel
+sed -e s%rfriendshomedir%$homedir%g smb4.conf_netbsd.skel > smb4.conf0
+sed -e s%rfriendsuser%$user%g smb4.conf0 > smb4.conf
+sudo cp -f smb4.conf $PREFIX/etc/samba/smb.conf
+rm smb4.conf0
+
+mkdir -p $homedir/smbdir/usr2/
+
+cat <<EOF > $homedir/rfriends3/config/usrdir.ini
+usrdir = "$homedir/smbdir/usr2/"
+tmpdir = "$homedir/tmp/"
+EOF
+
+# -----------------------------------------
+if [ $sys -eq 1 ]; then
+  sudo systemctl enable $smbd
+  sudo systemctl restart $smbd
+  sudo systemctl status $smbd
+else 
+  sudo rcctl start smbd
+  sudo rcctl check smbd
+fi
+
+exit 0

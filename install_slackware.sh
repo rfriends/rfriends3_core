2f78ea096ddfe75e038c8df566db911d448756fa
mapi <rfriends2017@ymail.ne.jp>
2026-06-12 19:00:50 +0900

    Update install_slackware.sh

--- install_slackware.sh
+++ install_slackware.sh
@@ -36,7 +36,7 @@ if [ $? != 0 ]; then
   exit 1
 fi
 # -----------------------------------------
-sudo sbopkg--yes
+sudo sbopkg -r --yes
 
 OUTPUT=$(sudo slackpkg update 2>&1)
 if echo "$OUTPUT" | grep -iq "GPG signature.*failed"; then
